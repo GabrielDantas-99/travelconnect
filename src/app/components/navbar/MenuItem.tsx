af8a964e455040407aef2155279ced4ccb874599
Gabriel  Felipe <previsibleemail@gmail.com>
2023-06-14 13:32:11 -0300

    Commit inicial + criacao do navmenu

---
+++ src/app/components/navbar/MenuItem.tsx
@@ -0,0 +1,14 @@
+'use client';
+
+interface MenuItemProps {
+    onClick: () => void;
+    label: string;
+}
+
+const MenuItem = () => {
+    return (
+        <></>
+    );
+}
+ 
+export default MenuItem;
